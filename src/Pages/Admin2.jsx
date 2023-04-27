3f3365948b5916744b67d1ac185e8bcb1e136d1a
kishore-Kumar-SK <kishore.kumar@newstreettech.com>
2023-04-27 15:08:38 +0530

    3rd api call

--- src/Pages/Admin2.jsx
+++ src/Pages/Admin2.jsx
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Dialog, DialogContent, Grid } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 import Button from "@mui/material/Button";
 import React from "react";
@@ -19,7 +19,22 @@ import Pokicall from "../Api/PokiApi";
 const Admin1 = () => {
   const [poki, setPoki] = useState([]);
   const [details, setDetails] = useState([]);
-  const dummy = [];
+  const [ability1, setAbility1] = useState([]);
+  const [ability2, setAbility2] = useState([]);
+  const [ind, setInd] = useState([]);
+  const [open, setOpen] = React.useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleClickOpen = (item, index) => {
+    setInd(index);
+    setSelectedItem(item);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   useEffect(() => {
     const listgen = () => {
       Pokicall.listgen()
@@ -30,50 +45,71 @@ const Admin1 = () => {
           console.error("Error: ", error);
         });
     };
-    const pokemonDetails = (url) => {
-      Pokicall.pokemonDetails(url)
-        .then((res) => {
-          dummy.push({
-            level: res?.data?.base_experience,
-            name: res?.data?.forms[0]?.name,
-            link: res?.data?.sprites?.other?.home?.front_default,
+    listgen();
+  }, []);
+  useEffect(() => {
+    if (poki.length > 0) {
+      const promises = poki.slice(0, 20).map((value) => {
+        return Pokicall.pokemonDetails(value.url);
+      });
+
+      const promAwait = async () => {
+        await Promise.all(promises)
+          .then((res) => {
+            setDetails(res);
+          })
+          .catch((error) => {
+            console.error("Error: ", error);
           });
-          setDetails(dummy);
+      };
+      promAwait();
+    }
+  }, [poki]);
+
+  useEffect(() => {
+    const promises1 = details.map((value) => {
+      return Pokicall.pokemonDetails(value?.data?.abilities[0]?.ability?.url);
+    });
+    const promises2 = details.map((value) => {
+      return Pokicall.pokemonDetails(value?.data?.abilities[1]?.ability?.url);
+    });
+    const abilityAwait = async () => {
+      await Promise.all([promises1, promises2])
+        .then((res) => {
+          setAbility1(res[0]);
+          setAbility2(res[1]);
         })
         .catch((error) => {
           console.error("Error: ", error);
         });
     };
-    listgen();
-    poki.forEach((value) => {
-      if (details.length < 20) {
-        pokemonDetails(value.url);
-      }
-    });
-  }, [poki.length]);
+    abilityAwait();
+  }, [details]);
 
-  const order = JSON.parse(JSON.stringify(details));
-  console.log("order", details);
-
+  console.log("order", ability1);
   function orderbyNoasc(order) {
     const sorted = Array.from(order).sort((a, b) =>
-      a.level > b.level ? 1 : -1
+      a.data?.base_experience > b.data?.base_experience ? 1 : -1
     );
     setDetails(sorted);
   }
   function orderbyNodes(order) {
     const sorted = Array.from(order).sort((a, b) =>
-      a.level < b.level ? 1 : -1
+      a.data?.base_experience < b.data?.base_experience ? 1 : -1
     );
     setDetails(sorted);
   }
 
   function orderbyNameasc(order) {
-    const sorted = Array.from(order).sort((a, b) => (a.name > b.name ? 1 : -1));
+    const sorted = Array.from(order).sort((a, b) =>
+      a.data?.forms[0]?.name > b.data?.forms[0]?.name ? 1 : -1
+    );
     setDetails(sorted);
   }
   function orderbyNamedes(order) {
-    const sorted = Array.from(order).sort((a, b) => (a.name < b.name ? 1 : -1));
+    const sorted = Array.from(order).sort((a, b) =>
+      a.data?.forms[0]?.name < b.data?.forms[0]?.name ? 1 : -1
+    );
     setDetails(sorted);
   }
   return (
@@ -143,24 +179,76 @@ const Admin1 = () => {
           <Grid key={index} item xs={12} sm={6} md={4} lg={3} minHeight="25%">
             <div align="center">
               <Card sx={{ maxWidth: 345 }}>
-                <CardActionArea>
+                <CardActionArea onClick={() => handleClickOpen(item, index)}>
                   <CardMedia
                     component="img"
                     height="200"
-                    image={item?.link}
+                    image={item?.data?.sprites?.other?.home?.front_default}
                     alt="img1"
                   />
                   <CardContent align="left">
                     <Stack>
                       <Typography gutterBottom variant="h5" component="div">
-                        Level:{item?.level}
+                        Level:{item?.data?.base_experience}
                       </Typography>
                       <Typography gutterBottom variant="h3" component="div">
-                        {item?.name}
+                        {item?.data?.forms[0]?.name}
                       </Typography>
                     </Stack>
                   </CardContent>
                 </CardActionArea>
+                <Dialog open={open} onClose={handleClose}>
+                  <DialogContent
+                    sx={{ display: "flex", flexDirection: "column" }}
+                  >
+                    <div align="center">
+                      <Typography variant="h2">
+                        {selectedItem?.data?.forms[0]?.name}
+                      </Typography>
+                      <img
+                        src={
+                          selectedItem?.data?.sprites?.other?.home
+                            ?.front_default
+                        }
+                        alt="ability"
+                      />
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <Typography variant="h4">
+                          {selectedItem?.data?.abilities[0]?.ability?.name}
+                        </Typography>
+                        {/* <Typography variant="h2">
+                          {
+                            ability1[ind]?.data
+                              ?.effect_entries[1]?.effect
+                          }
+                        </Typography> */}
+                      </div>
+                      <div
+                        style={{
+                          display: "flex",
+                          flexDirection: "column",
+                        }}
+                      >
+                        <Typography variant="h4">
+                          {selectedItem?.data?.abilities[1]?.ability?.name}
+                        </Typography>
+                      </div>
+                    </div>
+                  </DialogContent>
+                </Dialog>
               </Card>
             </div>
           </Grid>
